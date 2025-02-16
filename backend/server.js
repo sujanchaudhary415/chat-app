b5f0ba33628a9953b8829fc1a02a4958cb8e6793
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2025-02-16 14:30:19 +0545

    debbuged

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true  // enable set cookie in response header
+ 
+}));
 
 
 app.use('/api/auth',authRouter);
